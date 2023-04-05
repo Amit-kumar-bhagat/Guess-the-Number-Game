d6a7b4dfa61680f752aa87f6d1967f581bc79c05
Amit Kumar <129907778+Amit-kumar-bhagat@users.noreply.github.com>
2023-04-05 16:49:43 +0530

    Update script.js

--- Guess the Number Game/script.js
+++ Guess the Number Game/script.js
@@ -1,16 +1,5 @@
 'use strict';
-/*
-document.querySelector('.message').textContent;
-document.querySelector('.message').textContent = 'Correct Number';
 
-document.querySelector('.label-score').textContent;
-
-document.querySelector('.number').textContent = 12;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 56;
-console.log(document.querySelector('.guess').value);
-document.querySelector('.between').textContent = 'Between 1 and 20'; */
 
 const secretnum = Math.trunc(Math.random() * 20) + 1;
 
